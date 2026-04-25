915a055719f293d601a291f8488c75d0db9fb1e4
iwamotoco-source <iwamoto.co@ir-inc.net>
2026-04-26 01:41:08 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'pwa-v1';
+const ASSETS = [
+  '/appsbeta2026/',
+  '/appsbeta2026/index.html',
+  '/appsbeta2026/manifest.json',
+];
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/appsbeta2026/index.html')))
+  );
+});
